993b67b33cc20938b553a011716826679f6afdf7
Yongbin Min <contact@cichol.com>
2020-01-12 21:01:38 +0900

    Update dependencies

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,11 @@
 // Babel configuration.
 const presets = Object.entries({
-	'@babel/preset-env': { targets: { node: 'v10.15.1' } }
+	'@babel/preset-env': { targets: { node: 'v12.14.1' } }
 });
 
 const plugins = Object.entries({
-	'@babel/plugin-proposal-optional-catch-binding': {},
 	'@babel/plugin-proposal-export-default-from': {},
 	'@babel/plugin-proposal-export-namespace-from': {},
-	'babel-plugin-closure-elimination': {},
 	'@babel/plugin-transform-runtime': { regenerator: false }
 });
 
